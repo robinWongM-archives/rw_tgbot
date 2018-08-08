e0042e1ab94edc6af5783323e7a9be4e7bdd9d22
Robin Wong <chaowang0313@gmail.com>
2018-08-08 22:49:29 +0800

    add listen function

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const token = config.bot_token;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+let channels = []
+config.channels.forEach(channel => {
+    bot.getChat(channel).then(chat => {
+        channels.push({
+            id: channel,
+            name: chat.title,
+            previousCount: 0,
+            count: 0
+        })
+    })
+})
+
+// not ready to preserve the data - only for testing
+function fetchCount() {
+    for (let i = 0; i < channels.length; i++) {
+        let channel = channels[i]
+        bot.getChatMembersCount(channel.id).then(count => {
+            channel.count = count
+            if(channel.previousCount != 0 && channel.previousCount != count) {
+                // modified
+                let output = '【' + channel.name + '】分台订阅者人数更新：' + channel.previousCount + ' → ' + channel.count
+                bot.sendMessage(config.main_channel, output)
+            }
+            channel.previousCount = count
+            channels[i] = channel
+        })
+    }
+}
+
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
@@ -14,7 +43,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // of the message
 
   const chatId = msg.chat.id;
-  console.log(chatId)
+  if(chatId != config.owner_id) return
   const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
@@ -23,6 +52,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.onText(/\/chat (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
+    if(chatId != config.owner_id) return
     const resp = match[1]; // the captured "whatever"
 
     try {
@@ -33,3 +63,7 @@ bot.onText(/\/chat (.+)/, (msg, match) => {
         console.error(err)
     }
 })
+
+setInterval(() => {
+    fetchCount()
+}, 30000)
