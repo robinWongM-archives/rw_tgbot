e67d1f5a1da4b2e174c3e4a02083bd1cdf06d093
Robin Wong <chaowang0313@gmail.com>
2018-08-09 19:08:27 +0800

    don't know why crash

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const bot = new TelegramBot(token, {polling: true});
 let channels = []
 config.channels.forEach(category => {
     category.items.forEach(channel => {
+        console.log('@' + channel)
         bot.getChat('@' + channel).then(chat => {
             channels.push({
                 id: channel,
