a4128fef9f19eb045aa726ed5035ec70029eca41
Robin Wong <chaowang0313@gmail.com>
2018-08-15 20:42:30 +0800

    fix callback

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ bot.on('callback_query', async query => {
         
                 preList[channel.category].push({
                     text: channel.name,
-                    data: JSON.stringify({
+                    callback_data: JSON.stringify({
                         type: 'channel',
                         data: channel.id
                     })
@@ -586,22 +586,3 @@ init()
 }, 30000)
 
 fetchLatest() */
-
-
-// https://stackoverflow.com/a/14032965
-function exitHandler(options, exitCode) {
-    browser.close()
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
